77024bed97296db7079485764cdcc7327a1f78f2
DJ <social.deej@gmail.com>
2020-07-28 09:25:53 +0530

    Intializing Passport Setup

---
+++ Backend/config/passport-setup.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/user-model');
+const mongoose = require('mongoose');
+
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
+
+passport.use(
+    new GoogleStrategy({
+        // options for google strategy
+        clientID: /* your clientID*/"" ,
+        clientSecret:/*your clienSecret*/"",
+        callbackURL: '/auth/google/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+        // check if user already exists in our own db
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser){
+                // already have this user
+                console.log('user is: ', currentUser);
+                done(null, currentUser);
+            } else {
+                // if not, create user in our db
+                new User({
+					_id: new mongoose.Types.ObjectId(),
+                    googleId: profile.id,
+                    name: profile.displayName,
+                    email:profile._json.email
+                }).save().then((newUser) => {
+                    console.log('created new user: ', newUser);
+                    done(null, newUser);
+                });
+            }
+        });
+    })
+);
