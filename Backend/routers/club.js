e38e62fa95486a9646ba420b70611de308012983
DJ <social.deej@gmail.com>
2020-08-16 18:40:40 +0530

    Post Init For Clubs

--- Backend/routers/club.js
+++ Backend/routers/club.js
@@ -1,2 +1,9 @@
 const express = require('express')
+const ClubList = require('../models/club-list')
 const router = express.Router();
+
+
+// Route For Adding The Clubs --> Specifically To CC Members
+router.post('/addclub', async (req, res) => {
+    
+})
