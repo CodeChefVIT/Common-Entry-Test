7846e5d08e856940ed66cd725a2111535c8bf0fe
DJ <social.deej@gmail.com>
2020-08-16 17:19:49 +0530

    Enclsoing Try Exception

--- Backend/routers/questions.js
+++ Backend/routers/questions.js
@@ -5,23 +5,28 @@ const Moderate = require('../models/moderate-questions')
 const Easy = require('../models/easy-questions')
 
 // Route For Posting Question 
-router.post('/addquestion', async (req, res) => {
-    // Adding Status For The User Logged In 
+router.post('/addquestion', async (req, res) => { 
+    try {
+        // Adding Status For The User Logged In 
 
-    // End ( Temp Changes )
-    const {question, author, club, type, domain} = req.body
-    var addition = null ;
-    if (type == 'Easy'){
-        addition = new Easy({question, author, club, domain})
-        await addition.save()
-    } else if (type == 'Moderate'){
-        addition = new Moderate({question, author, club, domain})
-        await addition.save()
-    } else {
-        addition = new Moderate({question, author, club, domain})
-        await addition.save()
+        // End ( Temp Changes )
+        const {question, author, club, type, domain} = req.body
+        var addition = null ;
+        if (type == 'Easy'){
+            addition = new Easy({question, author, club, domain})
+            await addition.save()
+        } else if (type == 'Moderate'){
+            addition = new Moderate({question, author, club, domain})
+            await addition.save()
+        } else {
+            addition = new Difficult({question, author, club, domain})
+            await addition.save()
+        }
+        res.send(addition);
+    } catch (e) {
+        console.log(e);
+        res.send(e);
     }
-    res.send(addition);
 })
 
 // Route For Getting Question Bank 
