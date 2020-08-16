b8688b280735c20860fc12050ed0b8cca9d51e27
DJ <social.deej@gmail.com>
2020-08-16 16:00:07 +0530

    Completing the Schema

--- Backend/models/easy-questions.js
+++ Backend/models/easy-questions.js
@@ -1,1 +1,13 @@
-const mon
+const mongoose = require('mongoose')
+
+const easyQuestionSchema = mongoose.Schema({
+    question: {
+        type: String
+    },
+    author : {
+        type: String 
+    },
+    club :{
+        type: String 
+    }
+})
