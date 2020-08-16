535ec4c87f690f6c87d858ee0e6f366ed6070b48
DJ <social.deej@gmail.com>
2020-08-16 16:04:08 +0530

    Adding Schema

---
+++ Backend/models/hard-questions.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const difficultQuestionSchema = mongoose.Schema({
+    question: {
+        type: String
+    },
+    author : {
+        type: String 
+    },
+    club :{
+        type: String 
+    }
+})
+
+
+module.exports = mongoose.model('Difficult', moderateQuestionSchema)
