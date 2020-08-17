e4feffb6249b5cf2c359b6fa737fd8fdf5139097
DJ <social.deej@gmail.com>
2020-08-18 01:34:12 +0530

    Works on Selective Test Case

--- Backend/routers/attempt.js
+++ Backend/routers/attempt.js
@@ -1,4 +1,18 @@
-const express = require('express')
+const express = require('express');
+const easyQuestions = require('../models/easy-questions');
 const router = express.Router();
+
+// Importing Easy Question For Responses
+const General = require('../models/questions-general')
+
+router.post('/temproute/:id', async (req, res) => {
+    var id = req.params.id
+    const temp = await new General({easyQuestions : id})
+    console.log(temp)
+
+    let check = 
+
+    res.send(id);
+})
 
 module.exports = router ;
