d725c9965c3482e717d8e04f8c89130a86b436e9
DJ <social.deej@gmail.com>
2020-08-18 01:07:07 +0530

    Adding Question Bank Id DB

--- Backend/models/questions-general.js
+++ Backend/models/questions-general.js
@@ -2,6 +2,22 @@ const mongoose = require('mongoose');
 
 // Importing Sub Models Of The Schema 
 const Easy = require('./easy-questions')
-const Moderate = require()
+const Moderate = require('./moderate-questions')
+const Difficult = require('./difficult-questions')
 
-const QuestionSchema 
+const QuestionSchema = mongoose.Schema({
+    easyQuestions: [{
+        type: mongoose.ObjectId,
+        ref: 'Easy'
+    }],
+    moderateQuestions: [{
+        type: mongoose.ObjectId,
+        ref: 'Moderate'
+    }],
+    difficultQuestions: [{
+        type: mongoose.ObjectId,
+        ref: 'Difficult'
+    }]
+})
+
+module.exports = mongoose.model('Questions', QuestionSchema)
