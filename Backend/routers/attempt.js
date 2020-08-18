3e8483e23607b117340137d93892634ba1b54f0e
DJ <social.deej@gmail.com>
2020-08-19 03:01:41 +0530

    Making Reference to User

--- Backend/routers/attempt.js
+++ Backend/routers/attempt.js
@@ -8,11 +8,9 @@ const User = require('../models/user-model')
 router.post('/temproute/:id', async (req, res) => {
     var id = req.params.id
     
-    var ideasy = '5f3923908cc1c1d5d1ee557f'
-    var ideasy2 = '5f392a2e8cc1c1d5d1ee5580'
     try {
         const username = await User.findById(id);
-        const template = {easyIds: [ideasy, ideasy2], moderateIds:['5f392af88cc1c1d5d1ee5584'], difficultIds: ['5f392cc68cc1c1d5d1ee558a']}
+        const template = {easyIds: ['5f3923908cc1c1d5d1ee557f'], moderateIds:['5f392af88cc1c1d5d1ee5584'], difficultIds: ['5f392cc68cc1c1d5d1ee558a']}
         // console.log(template)
         username.questionsIds.push(template)
         await username.populate('questionsIds.easyIds').populate('questionsIds.moderateIds').populate('questionsIds.difficultIds').execPopulate()
