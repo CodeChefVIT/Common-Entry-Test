6e0759430460330f3c60ddfac097573036c35d7e
DJ <social.deej@gmail.com>
2020-08-20 01:02:24 +0530

    Importing Necessary Models

---
+++ Backend/routers/evalutate.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router();
+
+// Importing All The Necessary Models
+const User = require('../models/user-model')
+const Easy = require('../models/easy-questions')
+const Moderate = require ('../models/moderate-questions')
+const Difficult = require('../models/difficult-questions')
+
+
+// Route For Getting All The Students IDs with Responses 
+// Route For Posting Marks 
+// Route For Showing The Rank List of Student For Particular domain 
+
