7e3311b22b984bdab061ce68f9fea12d58ec2a87
DJ <social.deej@gmail.com>
2020-08-20 01:25:28 +0530

    Checking Test Cases

--- Backend/routers/evalutate.js
+++ Backend/routers/evalutate.js
@@ -14,7 +14,15 @@ const Difficult = require('../models/difficult-questions')
 
 // Route For Getting All The Student Responses with Ids --> Auth for Admins Only 
 router.get('/getresponses/:id', async (req, res) => {
-
+    var id = req.params.id 
+    try {
+        const user = await User.findById(id);
+        await user.easyresponses.populate('id').execPopulate();
+        res.send(user);
+    }catch (e){
+        console.log(e);
+        res.send(e);
+    }
 })
 
 module.exports = router ;
