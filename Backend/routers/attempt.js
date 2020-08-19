6d878d393f4da1527d06cf877f56b9b7b19f60b0
DJ <social.deej@gmail.com>
2020-08-19 22:43:08 +0530

    Iniiting Post Forwarding Of Questions

--- Backend/routers/attempt.js
+++ Backend/routers/attempt.js
@@ -15,6 +15,7 @@ const Difficult = require('../models/difficult-questions')
 // Route Displaying the Ids For an User 
 // Route For Storing The Response To an Id for an User
 
+// Route For Generating Questions Ids 
 router.post('/generatequestions/:id', async(req, res) => {
     try{
         const user = await User.findById(req.params.id)
@@ -74,7 +75,16 @@ router.post('/generatequestions/:id', async(req, res) => {
         
 })
 
-
+// Route For Displaying Questions 
+router.get('/questionsall/:id', async (req, res) => {
+    var id = req.params.id ;
+    try {
+        
+    }catch (e){
+        console.log(e);
+        res.send(e);
+    }
+})
 
 module.exports = router ;
 
