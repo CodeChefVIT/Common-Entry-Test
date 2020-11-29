f831061551ecee47e253674ab42642893c37d35e
Akshat Gupta <akshatvg23@gmail.com>
2020-11-29 22:31:46 +0530

    CET frontend init

---
+++ Frontend/common-entry-test/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Common Entry Test</h1>
+      </div>
+    )
+  }
+}
+
+export default App
