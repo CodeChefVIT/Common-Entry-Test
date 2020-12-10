74d0600439580d0f6736252a888dff2763fc7920
Akshat Gupta <akshatvg23@gmail.com>
2020-12-10 10:28:31 +0530

    components

--- Frontend/common-entry-test/src/App.js
+++ Frontend/common-entry-test/src/App.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import ClubList from './components/ClubList';
+import Deadlines from './components/Deadlines';
+import Exam from './components/Exam';
 
-export class App extends Component {
-  render() {
-    return (
+function App() {
+  return (
+    <BrowserRouter>
       <div className="App">
-        <h1>Common Entry Test</h1>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/list" component={ClubList} />
+          <Route exact path="/deadlines" component={Deadlines} />
+          <Route exact path="/exam/:exam_id" component={Exam} />
+        </Switch>
       </div>
-    )
-  }
+    </BrowserRouter>
+  )
 }
 
 export default App;
+// 282D33 black
+// E31E43 red
---
+++ Frontend/common-entry-test/src/components/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Switch, Typography, Button, AppBar, Toolbar, IconButton, Paper } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { useState } from 'react';
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles';
+
+const Home = () => {
+    const [darkMode, setDarkMode] = useState(true);
+    const lightTheme = createMuiTheme({
+        palette: {
+            type: "light",
+        },
+    });
+    const darkTheme = createMuiTheme({
+        palette: {
+            type: "dark",
+        },
+    });
+    return (
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+            <Paper style={{ height: "100vh" }} square={true} spacing={0}>
+                <div className="App">
+                    <AppBar position="static">
+                        <Toolbar>
+                            <IconButton edge="start" color="inherit" aria-label="menu">
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography variant="h6">
+                                News
+                </Typography>
+                            <Button color="inherit">Login</Button>
+                            <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+                        </Toolbar>
+                    </AppBar>
+                    <Typography variant="h2" component="h1">CET</Typography>
+                </div>
+            </Paper>
+        </ThemeProvider >
+    );
+}
+
+export default Home;
