83aa5ad7de6e45c13e6eb10098b8f277fed74a95
Akshat Gupta <akshatvg23@gmail.com>
2020-12-18 02:37:42 +0530

    tawkto

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,15 +31,15 @@
 })(jQuery);
 
 // tawk.to
-// var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-// (function () {
-//     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-//     s1.async = true;
-//     s1.src = 'https://embed.tawk.to/5fd2f14cdf060f156a8bf264/default';
-//     s1.charset = 'UTF-8';
-//     s1.setAttribute('crossorigin', '*');
-//     s0.parentNode.insertBefore(s1, s0);
-// })();
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/5fd2f14cdf060f156a8bf264/default';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
 
 // Pre Register
 document.getElementById('preRegForm').addEventListener('submit', (e) => {
